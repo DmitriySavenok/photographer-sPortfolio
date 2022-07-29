4398cdc6b0b8ecde1b0b1ea5e3b1c21d32026a2f
Савенок Дмитрий <36520766+DmitriySavenok@users.noreply.github.com>
2022-07-29 17:30:04 +0300

    navigation

--- src/js/components/header.js
+++ src/js/components/header.js
@@ -1,9 +1,11 @@
-function menuToggle() {
+function navigationToggle() {
 	let menuButton = document.querySelector(".header__menu-button");
+	let navigation = document.querySelector(".navigation");
 	menuButton.addEventListener('click', function() {
 		menuButton.classList.toggle('button__pressed');
+		navigation.classList.toggle('navigation--opened');
 		console.log('click');
 	})
 }
 
-menuToggle();
+navigationToggle();
