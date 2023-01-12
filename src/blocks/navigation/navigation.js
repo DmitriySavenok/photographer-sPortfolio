896f6f75850a8b3ab7df3a7a82d615a3e496979f
Савенок Дмитрий <36520766+DmitriySavenok@users.noreply.github.com>
2023-01-12 13:34:49 +0300

    header block title, fix position on mobile

--- src/blocks/navigation/navigation.js
+++ src/blocks/navigation/navigation.js
@@ -1,4 +1,6 @@
 function navigationFilter() {
+  let filter = '*';
+
   const elem = document.querySelector('.portfolio__grid');
   const iso = new Isotope( elem, {
     itemSelector: '.grid-item',
@@ -11,10 +13,29 @@ function navigationFilter() {
   if(pageTitle.classList.contains('index-page')) {
     document.querySelectorAll('.navigation__filter-btn').forEach(el => {
     el.addEventListener('click', (e) => {
-        let filter = e.currentTarget.dataset.filter;
+        filter = e.currentTarget.dataset.filter;
+        console.log(filter);
+
+
+        document.querySelectorAll('.gallery-photo').forEach(el => {
+          if (filter === '*') {
+            el.dataset.fancybox = 'gallery';
+          } else if (filter === '.vertical') {
+            document.querySelectorAll('.vert').forEach(vert => {
+              vert.dataset.fancybox = 'vertical';
+            })
+          } else if (filter === '.horizontal') {
+            document.querySelectorAll('.horiz').forEach(horz => {
+              horz.dataset.fancybox = 'horizontal';
+            })
+          }
+        })
+
+
+        // записываем в переменную id нажатой кнопки
         clickButton = "#"+el.id;
         localStorage.setItem('filterFocus', clickButton);
-        console.log(clickButton);
+        // console.log(clickButton);
         iso.arrange({filter: `${filter}`});
       });
     });
