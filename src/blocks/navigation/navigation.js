111a9d180bc44decf30c949d08e4a91563983680
Савенок Дмитрий <36520766+DmitriySavenok@users.noreply.github.com>
2023-01-16 13:53:57 +0300

    added close navigation after using filter

--- src/blocks/navigation/navigation.js
+++ src/blocks/navigation/navigation.js
@@ -1,6 +1,7 @@
 function navigationFilter() {
   let filter = '*';
 
+  // Инициализация библиотеки для плиточного расположения фото на страниц портфолио
   const elem = document.querySelector('.portfolio__grid');
   const iso = new Isotope( elem, {
     itemSelector: '.grid-item',
@@ -10,41 +11,80 @@ function navigationFilter() {
 
   // Фильтр фотографий на странице портфолио
   let pageTitle = document.querySelector('.body');
+
+  // Если открыта страница с фотографиями запускаем скрипт
   if(pageTitle.classList.contains('index-page')) {
+
+    // Цикл находит все кнопки с классом .navigation__filter-btn
     document.querySelectorAll('.navigation__filter-btn').forEach(el => {
-    el.addEventListener('click', (e) => {
-        filter = e.currentTarget.dataset.filter;
-        console.log(filter);
+      el.addEventListener('click', (e) => {
+          filter = e.currentTarget.dataset.filter;
+          // console.log(filter);
 
+          // Поиск фотографий по нажатой кнопке.
+          document.querySelectorAll('.gallery-photo').forEach(el => {
+            if (filter === '*') {
+              el.dataset.fancybox = 'gallery';
+            } else if (filter === '.vertical') {
+              document.querySelectorAll('.vert').forEach(vert => {
+                vert.dataset.fancybox = 'vertical';
+              })
+            } else if (filter === '.horizontal') {
+              document.querySelectorAll('.horiz').forEach(horz => {
+                horz.dataset.fancybox = 'horizontal';
+              })
+            }
+          })
 
-        document.querySelectorAll('.gallery-photo').forEach(el => {
-          if (filter === '*') {
-            el.dataset.fancybox = 'gallery';
-          } else if (filter === '.vertical') {
-            document.querySelectorAll('.vert').forEach(vert => {
-              vert.dataset.fancybox = 'vertical';
-            })
-          } else if (filter === '.horizontal') {
-            document.querySelectorAll('.horiz').forEach(horz => {
-              horz.dataset.fancybox = 'horizontal';
-            })
-          }
-        })
-
-
-        // записываем в переменную id нажатой кнопки
-        clickButton = "#"+el.id;
-        localStorage.setItem('filterFocus', clickButton);
-        // console.log(clickButton);
-        iso.arrange({filter: `${filter}`});
+          // записываем в переменную id нажатой кнопки
+          clickButton = "#"+el.id;
+          localStorage.setItem('filterFocus', clickButton);
+          console.log('В локальное хранилище в переменную filterFocus записано значение ' + clickButton);
+          iso.arrange({filter: `${filter}`});
       });
     });
+
+  // Иначе если открыта не страница портфолио (index.html), то по нажатии на кнопку фильтрации фото,
+  // произойдёт переход на страницу с портфолио и фотографии отсортируются так как было выбрано.
   } else {
     document.querySelectorAll('.navigation__filter-btn').forEach(el => {
       el.addEventListener('click', (e) => {
           window.location.href = 'index.html';
           clickButton = "#"+el.id;
           localStorage.setItem('filterFocus', clickButton);
+        });
+      });
+  }
+
+  // Скрипт отвечает за закрытие меню на мобильных экранах,
+  // после нажатия на какую-нибудь кнопку фильтра фотографий.
+
+  // Определяем, что экран меньше 768px
+  if (document.documentElement.clientWidth < 768) {
+
+    //записываем в перемнную "selectedNow" параметр фильтра выбранный в данный момент.
+    selectedNow = clickButton;
+    console.log('Сейчас выбрано ' + selectedNow);
+    console.log('Экран меньше 768px');
+
+    // отслеживаем нажатие на кнопку фильтра
+    document.querySelectorAll('.navigation__filter-btn').forEach(el => {
+      el.addEventListener('click', (e) => {
+
+        // если меню открыто и нажата кнопка фильтра то ...
+        if (document.querySelector(".navigation").classList.contains('navigation--opened')) {
+          console.log('В данный момент меню открыто');
+
+          // ... Если выбран пункт фильтра отличающейся от текущего,
+          // то закрыть меню и записать только что ыбранный пункт в текущий.
+            if (selectedNow !== clickButton) {
+              closeNavigation();
+              selectedNow = clickButton;
+            }
+
+          } else {
+            console.log('В данный момент меню закрыто');
+          }
         });
       });
   }
